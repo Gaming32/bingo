e6201a503ed5fbd677dc2e536a64ae1bb0de74e5
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2023-09-11 19:38:33 -0500

    Use ContextAwarePredicate for the ItemPickedUpTrigger

--- common/src/main/java/io/github/gaming32/bingo/triggers/ItemPickedUpTrigger.java
+++ common/src/main/java/io/github/gaming32/bingo/triggers/ItemPickedUpTrigger.java
@@ -6,6 +6,7 @@ import net.minecraft.advancements.critereon.*;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.level.storage.loot.LootContext;
 import org.jetbrains.annotations.NotNull;
 
 public class ItemPickedUpTrigger extends SimpleCriterionTrigger<ItemPickedUpTrigger.TriggerInstance> {
@@ -22,24 +23,25 @@ public class ItemPickedUpTrigger extends SimpleCriterionTrigger<ItemPickedUpTrig
     protected TriggerInstance createInstance(JsonObject json, ContextAwarePredicate predicate, DeserializationContext context) {
         return new TriggerInstance(
             predicate,
-            EntityPredicate.fromJson(json.get("item_entity"))
+            EntityPredicate.fromJson(json, "item_entity", context)
         );
     }
 
     public void trigger(ServerPlayer player, ItemEntity itemEntity) {
-        trigger(player, instance -> instance.matches(player, itemEntity));
+        final LootContext itemEntityContext = EntityPredicate.createContext(player, itemEntity);
+        trigger(player, instance -> instance.matches(itemEntityContext));
     }
 
     public static class TriggerInstance extends AbstractCriterionTriggerInstance {
-        private final EntityPredicate itemEntity;
+        private final ContextAwarePredicate itemEntity;
 
-        public TriggerInstance(ContextAwarePredicate predicate, EntityPredicate itemEntity) {
+        public TriggerInstance(ContextAwarePredicate predicate, ContextAwarePredicate itemEntity) {
             super(ID, predicate);
             this.itemEntity = itemEntity;
         }
 
         public static TriggerInstance pickedUp(EntityPredicate itemEntity) {
-            return new TriggerInstance(ContextAwarePredicate.ANY, itemEntity);
+            return new TriggerInstance(ContextAwarePredicate.ANY, EntityPredicate.wrap(itemEntity));
         }
 
         public static TriggerInstance pickedUpFrom(EntityPredicate droppedBy) {
@@ -60,12 +62,12 @@ public class ItemPickedUpTrigger extends SimpleCriterionTrigger<ItemPickedUpTrig
         @Override
         public JsonObject serializeToJson(SerializationContext context) {
             final JsonObject result = super.serializeToJson(context);
-            result.add("item_entity", itemEntity.serializeToJson());
+            result.add("item_entity", itemEntity.toJson(context));
             return result;
         }
 
-        public boolean matches(ServerPlayer player, ItemEntity itemEntity) {
-            return this.itemEntity.matches(player, itemEntity);
+        public boolean matches(LootContext itemEntity) {
+            return this.itemEntity.matches(itemEntity);
         }
     }
 }
