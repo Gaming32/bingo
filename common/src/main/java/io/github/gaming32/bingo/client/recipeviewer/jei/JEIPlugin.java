7bf9ba508577a9c896733da36c1fc30c41196d2e
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2024-04-18 19:15:29 -0500

    Remove event code in JEIPlugin

--- common/src/main/java/io/github/gaming32/bingo/client/recipeviewer/jei/JEIPlugin.java
+++ common/src/main/java/io/github/gaming32/bingo/client/recipeviewer/jei/JEIPlugin.java
@@ -1,8 +1,6 @@
 package io.github.gaming32.bingo.client.recipeviewer.jei;
 
 import com.mojang.blaze3d.platform.InputConstants;
-import dev.architectury.event.EventResult;
-import dev.architectury.event.events.client.ClientRawInputEvent;
 import io.github.gaming32.bingo.client.recipeviewer.RecipeViewerPlugin;
 import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.recipe.IFocus;
@@ -17,50 +15,6 @@ import java.util.List;
 import java.util.function.Function;
 
 public class JEIPlugin extends RecipeViewerPlugin {
-    private int viewRecipe = 0;
-    private int viewUsages = 0;
-
-    public JEIPlugin() {
-        ClientRawInputEvent.KEY_PRESSED.register((client, keyCode, scanCode, action, modifiers) -> {
-            final InputConstants.Key key = InputConstants.getKey(keyCode, scanCode);
-            if (action == InputConstants.PRESS) {
-                if (matchesKey(key, IJeiKeyMappings::getShowRecipe)) {
-                    viewRecipe++;
-                }
-                if (matchesKey(key, IJeiKeyMappings::getShowUses)) {
-                    viewUsages++;
-                }
-            } else if (action == InputConstants.RELEASE) {
-                if (matchesKey(key, IJeiKeyMappings::getShowRecipe)) {
-                    viewRecipe--;
-                }
-                if (matchesKey(key, IJeiKeyMappings::getShowUses)) {
-                    viewUsages--;
-                }
-            }
-            return EventResult.pass();
-        });
-        ClientRawInputEvent.MOUSE_CLICKED_PRE.register((client, button, action, mods) -> {
-            final InputConstants.Key key = InputConstants.Type.MOUSE.getOrCreate(button);
-            if (action == InputConstants.PRESS) {
-                if (matchesKey(key, IJeiKeyMappings::getShowRecipe)) {
-                    viewRecipe++;
-                }
-                if (matchesKey(key, IJeiKeyMappings::getShowUses)) {
-                    viewUsages++;
-                }
-            } else if (action == InputConstants.RELEASE) {
-                if (matchesKey(key, IJeiKeyMappings::getShowRecipe)) {
-                    viewRecipe--;
-                }
-                if (matchesKey(key, IJeiKeyMappings::getShowUses)) {
-                    viewUsages--;
-                }
-            }
-            return EventResult.pass();
-        });
-    }
-
     @Override
     public boolean isViewRecipe(InputConstants.Key key) {
         return matchesKey(key, IJeiKeyMappings::getShowRecipe);
