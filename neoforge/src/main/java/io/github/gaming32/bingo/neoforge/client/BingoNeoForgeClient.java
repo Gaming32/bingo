e14a91905a6f317ea4ee2c9a559cd73190649996
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2024-04-22 08:24:22 -0500

    Update NeoForge

--- neoforge/src/main/java/io/github/gaming32/bingo/neoforge/client/BingoNeoForgeClient.java
+++ neoforge/src/main/java/io/github/gaming32/bingo/neoforge/client/BingoNeoForgeClient.java
@@ -4,11 +4,11 @@ import io.github.gaming32.bingo.client.config.BingoConfigScreen;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModLoadingContext;
-import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
-@Mod.EventBusSubscriber(value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
+@EventBusSubscriber(value = Dist.CLIENT, bus = EventBusSubscriber.Bus.MOD)
 public class BingoNeoForgeClient {
     @SubscribeEvent
     public static void accept(FMLClientSetupEvent event) {
