efd7c6c28a0abb6d09f547cc311b54bc7ac8dace
Josiah Glosson <soujournme@gmail.com>
2024-11-27 10:58:54 -0600

    Update Parchment

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ subprojects {
             officialMojangMappings {
                 nameSyntheticMembers = true
             }
-            parchment("org.parchmentmc.data:parchment-1.21:2024.11.10@zip")
+            parchment("org.parchmentmc.data:parchment-1.21.1:2024.11.17@zip")
         })
 
         compileOnly("com.demonwav.mcdev:annotations:2.1.0")
