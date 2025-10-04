3a35ddf12da78415613c8eb27da2c8b29fead36c
joe <burtonjae@hotmail.co.uk>
2025-10-04 17:16:05 +0100

    Avoid ModifyReceiver because unimined is too buggy for it

--- src/fabric/java/io/github/gaming32/bingo/mixin/fabric/MixinServerLevel.java
+++ src/fabric/java/io/github/gaming32/bingo/mixin/fabric/MixinServerLevel.java
@@ -1,21 +1,15 @@
 package io.github.gaming32.bingo.mixin.fabric;
 
-import com.llamalad7.mixinextras.injector.ModifyReceiver;
 import io.github.gaming32.bingo.fabric.event.FabricEvents;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.ServerExplosion;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(ServerLevel.class)
 public class MixinServerLevel {
-    @ModifyReceiver(
-        method = "explode",
-        at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/world/level/ServerExplosion;explode()I"
-        )
-    )
+    @ModifyVariable(method = "explode", at = @At("STORE"))
     private ServerExplosion onExplosion(ServerExplosion instance) {
         FabricEvents.SERVER_EXPLOSION.invoker().accept((ServerLevel)(Object)this, instance);
         return instance;
