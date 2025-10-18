fbcea2f00aeb207905414dabedd4210e2b2d6da1
joe <burtonjae@hotmail.co.uk>
2025-10-18 16:20:16 +0100

    I know the alphabet yes

--- src/main/java/io/github/gaming32/bingo/data/tags/bingo/BingoItemTags.java
+++ src/main/java/io/github/gaming32/bingo/data/tags/bingo/BingoItemTags.java
@@ -14,13 +14,13 @@ public final class BingoItemTags {
     public static final TagKey<Item> BASIC_MINERAL_BLOCKS = create("basic_mineral_blocks");
     public static final TagKey<Item> BONEMEALABLE = create("bonemealable");
     public static final TagKey<Item> CLIMBABLE = create("climbable");
+    public static final TagKey<Item> COPPER_IN_NAME = create("copper_in_name");
     public static final TagKey<Item> DEAD_CORAL_BLOCKS = create("dead_coral_blocks");
     public static final TagKey<Item> DIAMOND_IN_NAME = create("diamond_in_name");
     public static final TagKey<Item> FISHING_JUNK = create("fishing_junk");
     public static final TagKey<Item> FISHING_TREASURE = create("fishing_treasure");
     public static final TagKey<Item> FISH_BUCKETS = create("fish_buckets");
     public static final TagKey<Item> GOLD_IN_NAME = create("gold_in_name");
-    public static final TagKey<Item> COPPER_IN_NAME = create("copper_in_name");
     public static final TagKey<Item> LIVING_CORAL_BLOCKS = create("living_coral_blocks");
     public static final TagKey<Item> MEAT = create("meat");
     public static final TagKey<Item> NOT_MEAT = create("not_meat");
